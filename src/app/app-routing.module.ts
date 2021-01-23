8c552ec40de474193a775483a050aaba771027a8
NelsonM9 <nextnelson1234@gmail.com>
2021-01-23 03:17:23 -0500

    Se crea la logica del componentes signin y login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', loadChildren: () => import ('./components/login/login.module').then(m => m.LoginModule) },
   { path: 'home', loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule) },
-  { path: 'login', loadChildren: () => import('./components/login/login.module').then(m => m.LoginModule) },
-  { path: 'signin', loadChildren: () => import('./components/signin/signin.module').then(m => m.SigninModule) },
-  { path: 'menu', loadChildren: () => import('./components/menu/menu.module').then(m => m.MenuModule) },
-  { path: 'myprofile', loadChildren: () => import('./components/myprofile/myprofile.module').then(m => m.MyprofileModule)},
-  { path: 'proceedings', loadChildren: () => import('./components/proceedings/proceedings.module').then(m => m.ProceedingsModule) },
-  { path: 'editprofile', loadChildren: () => import('./components/editprofile/editprofile.module').then(m => m.EditprofileModule) }];
+  { path: 'signin', loadChildren: () => import('./components/signin/signin.module').then(m => m.SigninModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { JsonManagerService } from '../../services/json-manager.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { SpinnerService } from 'src/app/services/spinner.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private rs: JsonManagerService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  url_login: string= 'http://127.0.0.1:5000/login';
+  dataEx: JSON;
+  state: string;
+  error: any;
+  login: FormGroup
 
   ngOnInit(): void {
+    this.login = this.fb.group({
+      document_u: ['', Validators.required],
+      password_u: ['', Validators.required]
+    })
+  }
+
+  onSubmit() {
+    this.rs.postData(this.url_login, this.login.value).subscribe((data: any) => {
+      this.dataEx = data;
+      localStorage.setItem("token",this.dataEx['token'])
+      this.state = this.dataEx['state'];
+      this.error = this.dataEx['error'];
+      switch (this.state) {
+        case 'welcome':
+          this.router.navigate(['/home'], {relativeTo: this.route});
+          console.log('Welcome')
+          break;
+        case 'document':
+            console.log('Incorrect Password')
+          break;
+        case 'password_u':
+          console.log('Incorrect Document')
+          break;
+        case 'error':
+          console.log('Error')
+        default:
+          console.log('Error')
+          break;
+      }
+    })
   }
 
 }
