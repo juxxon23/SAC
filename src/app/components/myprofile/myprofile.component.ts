9da3f8428877c9d0b103d2aff672d384d9c2023f
NelsonM01 <nelson.morales@atlanticsoft.us>
2021-03-07 17:36:24 -0500

    se completa el login

--- src/app/components/myprofile/myprofile.component.ts
+++ src/app/components/myprofile/myprofile.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+import { HttpToolService } from 'src/app/services/http-tool.service';
+
 
 @Component({
   selector: 'app-myprofile',
@@ -9,10 +13,58 @@ import { AuthService } from 'src/app/services/auth.service';
 export class MyprofileComponent implements OnInit {
 
   constructor(
+    private fb: FormBuilder,
+    private rs: HttpToolService,
+    private router: Router,
+    private route: ActivatedRoute,
     public auth : AuthService
   ) { }
 
+  url_myprofile: string = 'http://127.0.0.1:5000/signin';
+  dataEx: JSON;
+  state: string;
+  error: any;
+  myprofile: FormGroup;
+  stateSp: boolean = false;
+  habilitarEdit: boolean = false;
+  
+
   ngOnInit(): void {
+    this.myprofile = this.fb.group({
+      password_u: [''],
+      name_u: [''],
+      lastname_u: [''],
+      phone_u: [''],
+      regional_u: [''],
+      center_u: [''],
+      bonding_type: [''],
+      city_u:['']
+    })
+  }
+
+  click : boolean = false;
+
+  onButtonClick(){
+    this.click = !this.click;
+  }
+
+  onSubmit() {
+    this.stateSp = !this.stateSp;
+    var form = this.myprofile.value;
+    console.log(form);
+    this.rs.putRequest(this.url_myprofile, form).subscribe((data: any) => {
+      this.dataEx = data;
+      console.log(this.dataEx);
+      this.state = this.dataEx['state'];
+      switch (this.state) {
+        case 'ok':
+          this.router.navigate(['/myprofile'], { relativeTo: this.route});
+          break;
+        case ' error':
+          this.router.navigate(['/myprofile']);
+          break;
+      }
+    })
   }
 
 }
--- src/app/components/myprofile/myprofile.module.ts
+++ src/app/components/myprofile/myprofile.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { MyprofileRoutingModule } from './myprofile-routing.module';
 import { MyprofileComponent } from './myprofile.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [MyprofileComponent],
   imports: [
     CommonModule,
-    MyprofileRoutingModule
+    MyprofileRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class MyprofileModule { }
